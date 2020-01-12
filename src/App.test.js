6323c3dd3ec78b28bb9008365a6b6eb2189ff65b
Poutine <richardqin918@gmail.com>
2020-01-12 14:52:53 -0500

    initial comit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+=======
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+>>>>>>> 1ea7b50b6d66594f03ce2497593b8d4016ff279c
 });
